37faacccf7094f21b8a4c61a99de8e151cfdd987
Amashi Tharushika <amashitakesnotes03@gmail.com>
2026-08-15 20:36:55 +0530

    fix: correct KeyResolver import path in RateLimiterConfig

--- api-gateway/src/main/java/com/fms/api_gateway/config/RateLimiterConfig.java
+++ api-gateway/src/main/java/com/fms/api_gateway/config/RateLimiterConfig.java
@@ -1,6 +1,6 @@
 package main.java.com.fms.api_gateway.config;
 
-import org.springframework.cloud.gateway.route.builder.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
